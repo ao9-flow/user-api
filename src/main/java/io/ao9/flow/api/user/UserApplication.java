70d48e2f4c63cb8f8ddf7fbb95e429ce4e2bae63
Ao Dong <aodong99@gmail.com>
2020-05-18 17:39:55 -0400

    update

--- src/main/java/io/ao9/flow/api/user/UserApplication.java
+++ src/main/java/io/ao9/flow/api/user/UserApplication.java
@@ -4,11 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.client.RestTemplate;
 
 import feign.Logger;
 
@@ -27,11 +25,11 @@ public class UserApplication {
 		return new BCryptPasswordEncoder();
 	}
 
-	@Bean
-	@LoadBalanced
-	public RestTemplate getRestTemplate() {
-		return new RestTemplate();
-	}
+	// @Bean
+	// @LoadBalanced
+	// public RestTemplate getRestTemplate() {
+	// 	return new RestTemplate();
+	// }
 
 	@Bean
 	Logger.Level feignLoggerLevel() {
