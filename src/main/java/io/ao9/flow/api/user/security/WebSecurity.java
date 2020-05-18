c78843c73878b6d9f780c33a7ba838ef052d27ff
Ao Dong <aodong99@gmail.com>
2020-05-18 17:30:41 -0400

    update

--- src/main/java/io/ao9/flow/api/user/security/WebSecurity.java
+++ src/main/java/io/ao9/flow/api/user/security/WebSecurity.java
@@ -38,7 +38,6 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 
 	private AuthenticationFilter getAuthenticationFilter() throws Exception {
         AuthenticationFilter authenticationFilter = new AuthenticationFilter(env, userService, authenticationManager());
-        // authenticationFilter.setAuthenticationManager(authenticationManager());
         authenticationFilter.setFilterProcessesUrl(env.getProperty("login.url.path"));
         return authenticationFilter;
     }
